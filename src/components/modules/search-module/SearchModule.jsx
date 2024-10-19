8a3c90bfb0e8db2b559f828f61f33bb908f2f100
ikhodabande_fanwebco <amimhmdkh@gmail.com>
2024-10-19 13:39:50 +0330

    update adding some search modules

--- src/components/modules/search-module/SearchModule.jsx
+++ src/components/modules/search-module/SearchModule.jsx
@@ -7,18 +7,18 @@ const { Search } = Input;
 
 const onSearch = (value, _e, info) => console.log(info?.source, value);
 const SearchModule = () => (
-  //   <Search className="w-[340px]" allowClear placeholder="جستجو ..." onSearch={onSearch} enterButton />
-  <div>
-    <div className="w-[95vw] sm:w-[370px] h-10 border-2 border-gray-300 rounded-3xl flex items-center bg-background ">
-      <div className="w-9 h-9 bg-background flex items-center justify-center  rounded-full">
-        <IoSearch className="text-display-lg  text-gray-600" />
-      </div>
-      <input
-        type="text"
-        placeholder="جستجو ..."
-        className="w-full flex-1 outline-none border-none h-full rounded-full px-2 text-sm bg-background"
-      />
-    </div>
-  </div>
+    <Search className="w-[340px] shadow-lg " allowClear placeholder="جستجو ..." onSearch={onSearch} enterButton />
+  // <div>
+  //   <div className="w-[95vw] sm:w-[370px] h-10 border-2 border-gray-300 rounded-3xl flex items-center bg-background ">
+  //     <div className="w-9 h-9 bg-background flex items-center justify-center  rounded-full">
+  //       <IoSearch className="text-display-lg  text-gray-600" />
+  //     </div>
+  //     <input
+  //       type="text"
+  //       placeholder="جستجو ..."
+  //       className="w-full flex-1 outline-none border-none h-full rounded-full px-2 text-sm bg-background"
+  //     />
+  //   </div>
+  // </div>
 );
 export default SearchModule;
--- src/components/modules/search-module/search-section/SearchSection.jsx
+++ src/components/modules/search-module/search-section/SearchSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const SearchSection = () => {
   return (
@@ -6,9 +7,9 @@ export const SearchSection = () => {
      <div className='w-[150px] h-[150px] border bg-webcom-gradient rounded-xl relative'>
       <p className='text-white mt-4 mx-2 text-sm w-full'>محصولات فروشگاهی</p>
       <p className='text-white mx-2 text-lg font-iranyekanBold'>وبکام </p>
-        <div className='w-[90px] h-6 rounded-full bg-white absolute bottom-2 right-1 shadow-lg '>
+        <Link className='w-[90px] h-6 rounded-full bg-white absolute bottom-2 right-1 shadow-lg '>
            <p className='text-xs w-full text-center font-iranyekanBold hover:cursor-pointer '>بیشتر ببینید</p>
-        </div>
+        </Link>
      </div>
     </>
   )
